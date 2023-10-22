4d5d59f69270bebd5e9d5bee23c2e71a9889354f
Joseph-Carter <1joseph1carter@gmail.com>
2023-10-22 19:39:53 -0400

    adding live server

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,8 +5,8 @@ const Homepage = () => {
     <div className="Homepage">
       <img src="src/assets/CarterCapImage.jpg" className="homeimage"></img>
       <div className="Welcome">
-        <h2>Mobile Banking</h2>
-        <h1>We're just a tap away!</h1>
+        <h2>Welcome to Carter Capital</h2>
+        <h1>Best place for budgeting and keeping track of your money!!</h1>
         <h3>
           Stay on top of your expenses wherever you go. All the great features
           of online budgeting in the palm of your hand.
