2045cab2649c53d367aff532a63f6e79d48d9f66
Joseph-Carter <1joseph1carter@gmail.com>
2023-10-28 19:17:32 -0400

    adds favicons

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
       <img src="public/CarterCapImage.jpg" className="homeimage"></img>
       <div className="Welcome">
         <h2>Welcome to Carter Capital</h2>
-        <h1>Best place for budgeting and keeping track of your money!!</h1>
+        <h1>Best place for budgeting and keeping track of your money!</h1>
         <h3>
           Stay on top of your expenses wherever you go. All the great features
           of online budgeting in the palm of your hand.
